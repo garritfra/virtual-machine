954b46122bd61bbda71295a0764eeb49f839fbb7
garritfra <garritfranke@gmail.com>
2018-12-11 18:20:58 +0100

    Add Hello world in c

--- logicgates.js
+++
@@ -1,7 +0,0 @@
-const gates = {};
-
-gates.and = (a, b) => !!a && !!b;
-gates.or = (a, b) => !!a || !!b;
-gates.not = a => !a;
-
-module.exports = gates;
--- sandbox.js
+++
@@ -1,5 +0,0 @@
-const { and, or, not } = require("./logicgates");
-
-const sum = and(0, 0);
-
-console.log(sum);
