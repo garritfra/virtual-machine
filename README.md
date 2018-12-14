53ecec0b6a272634423b35da547d93a9bc80ddca
garritfra <garritfranke@gmail.com>
2018-12-14 16:06:51 +0100

    Add op_push and op_print

---
+++ README.md
@@ -0,0 +1,9 @@
+## Op-Codes
+
+| Instruction | Bytes | Description                                        |
+| :---------- | :---- | :------------------------------------------------- |
+| nop         | 0x00  | Do nothing                                         |
+| halt        | 0xff  | Stop execution                                     |
+| push        | 0x01  | Push the next token onto the stack                 |
+| print       | 0x02  | Pops a value off the stack and prints it to stdout |
+| add         | 0x03  | Add two integers on top of the stack               |
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let instructions = vec![0x00, 0x00, 0xff];
+    let instructions = vec![0x01, 0x10, 0x01, 0x11, 0x02, 0x02, 0xff];
     let mut vm = VM::new();
     vm.load_instructions(instructions);
     vm.run();
@@ -9,10 +9,25 @@ fn main() {
 struct Stack {
     top: u32,
     size: u32,
+    stack: Vec<i32>,
 }
 impl Stack {
     fn new(size: u32) -> Stack {
-        Stack { top: 0, size: size }
+        Stack {
+            top: 0,
+            size: size,
+            stack: Vec::new(),
+        }
+    }
+
+    fn push(&mut self, value: i32) {
+        self.stack.push(value);
+        self.top += 1;
+    }
+
+    fn pop(&mut self) -> Option<i32> {
+        self.top -= 1;
+        self.stack.pop()
     }
 }
 
@@ -38,28 +53,39 @@ impl VM {
         self.instructions = instructions;
     }
 
-    fn fetch(&mut self) {
-        self.ip += 1;
-    }
-
     fn run(&mut self) {
         self.running = true;
         while self.running {
             match self.instructions[(self.ip) as usize] {
                 0x00 => self.op_nop(),
+                0x01 => self.op_push(),
+                0x02 => self.op_print(),
                 0xff => self.op_halt(),
                 _ => self.op_halt(),
             }
-            self.fetch();
         }
     }
 
     fn op_nop(&mut self) {
-        println!("NO OP");
+        self.ip += 1;
     }
 
     fn op_halt(&mut self) {
-        println!("HALT");
         self.running = false;
+    }
+
+    fn op_push(&mut self) {
+        self.ip += 1;
+        let value = self.instructions[(self.ip) as usize] as i32;
+        self.stack.push(value);
+        self.ip += 1;
+    }
+
+    fn op_print(&mut self) {
+        match self.stack.pop() {
+            Some(v) => println!("{}", v),
+            None => println!("Error printing value"),
+        }
+        self.ip += 1;
     }
 }
