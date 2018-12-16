b94fc438461afe0c72b401e1187f4dd0b9b70e5d
garritfra <garritfranke@gmail.com>
2018-12-16 12:52:03 +0100

    Add file parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+mod parser;
 mod stack;
 mod vm;
 
 use vm::VM;
 
 fn main() {
-    let instructions = vec![0x01, 0x10, 0x01, 0x11, 0x03, 0x02, 0xff];
+    let instructions = parser::parse_file("./example.dasm".to_string()).unwrap();
     let mut vm = VM::new();
     vm.load_instructions(instructions);
     vm.run();
---
+++ src/parser.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::str;
+
+pub fn parse_file(path: String) -> Result<Vec<u8>, std::io::Error> {
+  let mut instructions: Vec<u8> = Vec::new();
+  let input = File::open(path)?;
+  let buffered = BufReader::new(input);
+
+  for line in buffered.lines() {
+    match line.unwrap().as_ref() {
+      "nop" => instructions.push(0x00),
+      "push" => instructions.push(0x01),
+      "print" => instructions.push(0x02),
+      "add" => instructions.push(0x03),
+      "halt" => instructions.push(0xff),
+      x => instructions.push(u8_from_utf8(x)),
+    }
+  }
+
+  Ok(instructions)
+}
+
+fn u8_from_utf8(string: &str) -> u8 {
+  std::str::from_utf8(string.as_bytes())
+    .unwrap()
+    .parse()
+    .unwrap()
+}
